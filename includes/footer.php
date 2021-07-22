11a5dcb692ae9c30b62061ab8e97728e333fb6e1
Nicopx <nicolas.pat@hotmail.com>
2021-07-22 14:00:04 -0300

    algo

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,12 @@
-    <div id="footer">
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container">
+  <div id="footer">
         <?php 
             echo "<p class='text-center'> Copyright " . date("Y"). " - <a href='https://github.com/Nicopx'>Nicopx</a></p>"
         ?>
     </div>    
+  </div>
+</footer>
 </div>
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
