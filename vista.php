62f7cb0170d8784b9b469bf1ff503bbf223eceaf
Nicopx <nicolas.pat@hotmail.com>
2021-07-22 10:55:50 -0300

    vista de detalles

--- vista.php
+++ vista.php
@@ -3,39 +3,38 @@
     require_once 'includes/header.php';
     require_once 'db/conn.php';
 
-    //traigo los asistentes 
-    //$resultado = $crud->verAsistentes();
-    if (isset($_POST['submit'])) {
-        //guardos los valores del arrat $_POST
-        $nombre = $_POST['input_nombre'];
-        $apellido = $_POST['input_apellido'];
-        $fecha_n = $_POST['fecha_nacimiento'];
-        $email = $_POST['email'];
-        $celular = $_POST['numero_contacto'];
-        $especialidad = $_POST['especialidad'];
-        //llamo a la funcion insert 
-        $isSuccess = $crud->insert($nombre, $apellido, $fecha_n, $email, $celular, $especialidad);
-
-        if ($isSuccess) {
-           echo '<h1 class="text-center text-success">¡Registración Existosa!</h1>';
-        }
-        else {
-            echo '<h1 class="text-center text-danger">Error procesando la solicitud!</h1>';
-        }
-    }
+    //traigo los asistentes por id
+    //si el id no existe se muestra el mensaje de error.
+    if (!isset($_GET['id'])) {
+        echo '<h1 class="text-center text-danger">Por favor ver detalles e intentar de nuevo</h1>';
+    }else {
+        //si no, se renderiza la card.
+        $id = $_GET['id'];
+        $resultado = $crud->verDetallesAsistentes($id);
+    
 ?>
 
-<?php ?>
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title"><?php echo $_POST['input_nombre']. ' ' . $_POST['input_apellido'];?></h5>
-        <h6 class="card-subtitle mb-2 text-muted">Email: <?php echo $_POST['email'] ?></h6>
-        <h6 class="card-subtitle mb-2 text-muted">Especialidad: <?php echo $_POST['especialidad'] ?></h6>
-        <h6 class="card-subtitle mb-2 text-muted">Fecha de Nacimiento: <?php echo $_POST['fecha_nacimiento'] ?></h6>
-        <h6 class="card-subtitle mb-2 text-muted">Contacto: <?php echo $_POST['numero_contacto'] ?></h6>
-        <a href="#" class="card-link">GitHub</a>
+    <?php ?>
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">
+                <?php echo $resultado['nombres']. ' ' . $resultado['apellido'];?>
+            </h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+                Email: <?php echo $resultado['email'] ?>
+            </h6>
+            <h6 class="card-subtitle mb-2 text-muted">
+                Especialidad: <?php echo $resultado['nombre'] ?>
+            </h6>
+            <h6 class="card-subtitle mb-2 text-muted">
+                Fecha de Nacimiento: <?php echo $resultado['fecha_nacimiento'] ?>
+            </h6>
+            <h6 class="card-subtitle mb-2 text-muted">
+                Contacto: <?php echo $resultado['celular'] ?>
+            </h6>
+            <a href="#" class="card-link">GitHub</a>
+        </div>
     </div>
-    </div>
-
+<?php } ?>
 
 <?php require_once 'includes/footer.php';?>
