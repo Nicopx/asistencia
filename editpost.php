fc50574b2e43ece672e85b20ca5e2c705ef243a7
Nicopx <nicolas.pat@hotmail.com>
2021-07-23 14:48:43 -0300

    script para editar

---
+++ editpost.php
@@ -0,0 +1,28 @@
+<?php 
+    require_once 'db/conn.php';
+
+    //guardo los valores del metodo post
+    if (isset($_POST['submit'])) {
+        
+        $id = $_POST['asistente_id'];
+        $nombre = $_POST['input_nombre'];
+        $apellido = $_POST['input_apellido'];
+        $fecha_n = $_POST['fecha_nacimiento'];
+        $email = $_POST['email'];
+        $celular = $_POST['numero_contacto'];
+        $especialidad = $_POST['especialidad'];
+
+    //llamo a la funcion del crud
+    $resultado = $crud->editarAsistente($id, $nombre, $apellido, $fecha_n, $email, $celular, $especialidad);
+
+    //redirijo a index.php
+    if($resultado){
+            header("Location: index.php");
+    }
+
+    }else {
+        echo 'error';
+    }
+
+
+?>
