c5da8198d839f5e30d86a11e8d558c7acf0f6e20
Nicopx <nicolas.pat@hotmail.com>
2021-08-09 12:18:56 -0300

    modificaciones generales

--- db/conn.php
+++ db/conn.php
@@ -1,18 +1,22 @@
 <?php 
     //conexion local con la base de datos usando PDO 
-    /*$host = '127.0.0.1'; //localhost
+    $host = '127.0.0.1'; //localhost
     $db = 'asistencia_db';
     $user = 'root';
     $pass = '';
-    $charset = 'utf8mb4';*/
+    $charset = 'utf8mb4';
 
 
     //Conexion remota 
-    $host = 'remotemysql.com'; //localhost
+/*     $host = 'remotemysql.com'; //localhost
     $db = 'Uit8L1iPnO';
     $user = 'Uit8L1iPnO';
     $pass = 'kTzU4yYSmo';
-    $charset = 'utf8mb4';
+    $charset = 'utf8mb4'; */
+
+
+    //https://www.freesqldatabase.com/freemysqldatabase/
+    //https://www.freemysqlhosting.net/register/?action=register
 
     //dsn contiene los datos de conexión.
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset;";
@@ -30,5 +34,9 @@
 
     //cuando la conexion es exitosa referecion crud e instancio la clase y
     require_once 'crud.php';
+    require_once 'user.php';
     $crud = new crud($pdo);
+    $user = new user($pdo);
+    //cargo el usuario admin por primera vez
+    $user->insertUser("admin", "admin");
 ?>
