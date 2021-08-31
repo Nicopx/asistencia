39365df55fa6cf0055df46451427fd03670facd3
Nicopx <nicolas.pat@hotmail.com>
2021-08-31 11:33:47 -0300

    modificacion de card

--- vista.php
+++ vista.php
@@ -14,9 +14,11 @@
     
 ?>
 
-    <?php ?>
-    <div class="container">
+ 
+    <div class="card-vista">
         <div class="card" style="width: 18rem;">
+                                        <?php #si no hay imagen dentro de ulploads se muestra una blanck por default?>
+        <img class="card-img-top" src="<?php echo  empty($resultado['avatar_path']) ? "uploads/blank.jpg" : $resultado['avatar_path']; ?>"/> 
             <div class="card-body">
                 <h5 class="card-title">
                     <?php echo $resultado['nombres']. ' ' . $resultado['apellido'];?>
@@ -33,15 +35,13 @@
                 <h6 class="card-subtitle mb-2 text-muted">
                     Contacto: <?php echo $resultado['celular'] ?>
                 </h6>
-                <a href="#" class="card-link">GitHub</a>
+                <br>
+                <a href="ver_registrados.php" class="btn btn-info">Volver</a> 
+                <!--- le paso el id del la persona registrada para ver sus detalles-->
+                <a href="editar.php?id=<?php echo $resultado['id']?>" class="btn btn-warning">Editar</a>
+                <a onclick="return confirm('¿Seguro que desea eliminar este registro?');" href="eliminar.php?id=<?php echo $resultado['id']?>" class="btn btn-danger">Eliminar</a>
             </div>
         </div>
-    
-        <br>
-        <a href="ver_registrados.php" class="btn btn-info">Volver</a> 
-        <!--- le paso el id del la persona registrada para ver sus detalles-->
-        <a href="editar.php?id=<?php echo $resultado['id']?>" class="btn btn-warning">Editar</a>
-        <a onclick="return confirm('¿Seguro que desea eliminar este registro?');" href="eliminar.php?id=<?php echo $resultado['id']?>" class="btn btn-danger">Eliminar</a>
     </div>
 <?php } ?>
 
