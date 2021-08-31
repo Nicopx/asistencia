4e5dac7e79f10e035ae12c9b7f7868adc2efd9f0
Nicopx <nicolas.pat@hotmail.com>
2021-08-31 11:36:59 -0300

    agregado de input para imagenes

--- index.php
+++ index.php
@@ -18,7 +18,9 @@
 
     -->
 <div class="container">
-    <form method="post" action="success.php"> 
+
+
+    <form method="post" action="success.php" enctype="multipart/form-data"> 
         <div class="mb-3">
             <label for="nombre" class="form-label">Nombre</label>
             <input required type="text" class="form-control" id="input_nombre" name="input_nombre">
@@ -41,18 +43,23 @@
             </select>
         </div>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Direccón de Email</label>
+            <label for="email" class="form-label">Direccón de Email</label>
             <input required type="email" class="form-control" id="email" name="email">
             <div id="emailHelp" class="form-text">Tu dirección de email no se compartira con nadie.</div>
         </div>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Número de contacto</label>
+            <label for="celular" class="form-label">Número de contacto</label>
             <input type="text" class="form-control" id="numero_contacto" name="numero_contacto">
             <div id="numero_ayuda" class="form-text">Tu número de teléfono no se compartira con nadie.</div>
         </div>
+        <div class="custom-file">
+            <label for="avatar" class="form-label">Subir Imágen (opcional)</label>
+            <input class="form-control" type="file" accept="image/*" id="avatar" name="avatar">
+        </div>
+        <br>
         <button type="submit" name="submit" class="btn btn-dark">Submit</button>
     </form>
 </div>
 
-<?php require_once 'includes/footer.php';?>
+<?php include_once 'includes/footer.php';?>
   
