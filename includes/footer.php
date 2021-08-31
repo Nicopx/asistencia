8b9cdf3e9a845051c21bd0343cf33f4b42daccc1
Nicopx <nicolas.pat@hotmail.com>
2021-08-31 11:37:35 -0300

    unificacion de estilos

--- includes/footer.php
+++ includes/footer.php
@@ -1,33 +1,32 @@
-<br>
-<footer class="footer mt-auto py-3 bg-light">
-  <div class="container">
-  <div id="footer">
-        <?php 
-            echo "<p class='text-center'> Copyright " . date("Y"). " - <a href='https://github.com/Nicopx'>Nicopx</a></p>"
-        ?>
+
+  <footer id="footer" class="footer mt-auto py-3 bg-dark text-white fixed-bottom">
+    <div id="footer-content">
+          <?php 
+              echo "<p class='text-center'> Copyright " . date("Y"). " - <a href='https://github.com/Nicopx'>Nicopx</a></p>"
+          ?>
     </div>    
-  </div>
-</footer>
+  </footer>
 </div>
+
   <!-- Option 1: Bootstrap Bundle with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script>
-      $( function() {
-        $( "#fecha_nacimiento" ).datepicker({
-          changeMonth: true,
-          changeYear: true,
-          dateFormat: "yy-mm-dd",
-          yearRange: "-100:+0"
-        })
-        ;
-      });
-</script>
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <script>
+        $( function() {
+          $( "#fecha_nacimiento" ).datepicker({
+            changeMonth: true,
+            changeYear: true,
+            dateFormat: "yy-mm-dd",
+            yearRange: "-100:+0"
+          })
+          ;
+        });
+  </script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -19,35 +19,36 @@ include_once 'includes/session.php';
     <title>Asistencia - <?php echo $titulo?></title>
   </head>
   <body>
-    <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">Conferencia IT</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav ml-auto">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php">Conferencia IT</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ml-auto">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              <?php 
+              //Si la sesion de usuario está existe se muestra el link para ver los asistentes
+              if (isset($_SESSION['id'])) {
+              ?>
+                <a class="nav-link" href="ver_registrados.php">Ver Asistentes</a>
+              <?php } ?>
+            </div>
+          </div>
+          <div class="navbar-nav mr-auto">
             <?php 
-            //Si la sesion de usuario está existe se muestra el link para ver los asistentes
-            if (isset($_SESSION['id'])) {
+              if (!isset($_SESSION['id'])) {
             ?>
-              <a class="nav-link" href="ver_registrados.php">Ver Asistentes</a>
-            <?php } ?>
+              <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+            <?php }else {?>
+              <a class="nav-link active" aria-current="page" href="#"><span class="text-light bg-dark">Hola <?php echo $_SESSION['nombre_usuario']?>!</span></a>
+              <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
+            <?php }?>
+            </div>
           </div>
         </div>
-        <div class="navbar-nav mr-auto">
-          <?php 
-            if (!isset($_SESSION['id'])) {
-          ?>
-            <a class="nav-link active" aria-current="page" href="login.php">Login</a>
-          <?php }else {?>
-            <a class="nav-link active" aria-current="page" href="#"><span class="text-light bg-dark">Hola <?php echo $_SESSION['nombre_usuario']?>!</span></a>
-            <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
-          <?php }?>
-          </div>
-        </div>
-      </div>
-    </nav>
-<br>
+      </nav>
+    <div class="container">
+      
+      <br>
